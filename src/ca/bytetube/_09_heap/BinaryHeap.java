532a155033bae06a3b56ab74f2a8276d0ff08bdd
isisisisisisithch <dallucus@gmail.com>
2022-11-04 20:45:08 -0400

    added heap add

--- src/ca/bytetube/_09_heap/BinaryHeap.java
+++ src/ca/bytetube/_09_heap/BinaryHeap.java
@@ -1,36 +1,105 @@
 package ca.bytetube._09_heap;
 
-import java.io.Serializable;
-
+//max heap
 public class BinaryHeap {
-   // Serializable
+
+    private Integer[] elements;
+    private int size;//nums of elements
+    public static final int DEFAULT_CAPACITY = 10;
+
+    public BinaryHeap() {
+        elements = new Integer[DEFAULT_CAPACITY];
+    }
+
     // the number of elements
     int size() {
-        return 0;
+        return size;
     }
 
 
     // is it empty
     boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
 
     // clear elements
     void clear() {
-
+//        for (int i = 0; i < elements.length; i++) {
+//            elements[i] = null;
+//        }
+        size = 0;
     }
 
 
     // add elements
     void add(Integer element) {
+        if (element == null) throw new NullPointerException("element can not be null !");
+        extendCapacity(size + 1);
+        elements[size++] = element;
+        siftUp(size - 1);
 
+    }
+
+    private void extendCapacity(int numsOfElements) {
+        int oldCapacity = elements.length;
+        if (oldCapacity >= numsOfElements) return;
+        //1.5
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        Integer[] newElements = new Integer[newCapacity];
+
+        //move data from elements to newElements
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+
+        elements = newElements;
+
+
+    }
+
+    private void siftUp(int index) {
+        Integer element = elements[index];
+        while (index > 0) {
+            int parentIndex = (index - 1) >> 1;
+            Integer parent = elements[parentIndex];
+            //If node ≤ parent node
+            if (element <= parent) break;
+
+            //If node ＞ parent node
+            elements[index] = parent;
+            index = parentIndex;
+
+        }
+        elements[index] = element;
+
+
+//        if (index!=0) {
+//            int parentI = (index - 1) >> 1;
+//            while (elements[index] > elements[parentI] && index != 0) {
+//                parentI = (index - 1) >> 1;
+//                int temp = elements[index];
+//                elements[index] = elements[parentI];
+//                elements[parentI] = temp;
+//
+//                index = parentI;
+//
+//
+//
+//            }
+//        }
     }
 
 
     // get the top element of the heap
     Integer get() {
-        return 0;
+        emptyCheck();
+        return elements[0];
+
+    }
+
+    private void emptyCheck() {
+        if (size == 0) throw new RuntimeException("heap is empty!");
     }
 
 
--- src/ca/bytetube/_09_heap/Main.java
+++ src/ca/bytetube/_09_heap/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         BinaryHeap heap = new BinaryHeap();
+        int[] arr = {72, 68 ,50 ,43 ,38 ,47 ,21, 14 ,80};
+        for (int i = 0; i <arr.length ; i++) {
+            heap.add(arr[i]);
+        }
 
-        heap.add(10);
+        System.out.println("end");
     }
 }
